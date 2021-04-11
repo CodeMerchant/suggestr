d001c541027afd40e32bb0def65cf5c54b3b8bbd
CodeMerchant <mb.msomi@gmail.com>
2021-04-11 03:45:29 +0200

    refactored line 26.

--- src/main/java/com/codemerchant/suggestr/utilities/AuditAwareImpl.java
+++ src/main/java/com/codemerchant/suggestr/utilities/AuditAwareImpl.java
@@ -1,9 +1,10 @@
 package com.codemerchant.suggestr.utilities;
 
-import com.codemerchant.suggestr.model.User;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -18,7 +19,7 @@ public class AuditAwareImpl implements AuditorAware<String> {
     public Optional<String> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        if (authentication == null || authentication.isAuthenticated()) {
+        if (authentication == null || !authentication.isAuthenticated()) {
             return Optional.empty();
         }
 
